47df68ba0b45f412b3e8a50a038f696b45068dfe
Hakim Rouatbi <hakim.rouatbi-ext@pernod-ricard.com>
2017-01-10 01:27:50 +0000

    fix images

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 	<div class="row">
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/e.jpeg">
+	      <img src="img/e.jpg">
 	      <div class="caption">
 		<h3>Emily</h3>
 		<p><a href="thanks.php" class="btn btn-success" role="button">Vote</a></p>
@@ -39,7 +39,7 @@
 
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/t.jpeg">
+	      <img src="img/t.jpg">
 	      <div class="caption">
 		<h3>Tori</h3>
 		<p><a href="thanks.php" class="btn btn-primary" role="button">Vote</a></p>
@@ -49,7 +49,7 @@
 <!--
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/k.jpeg">
+	      <img src="img/k.jpg">
 	      <div class="caption">
 		<h3>Kayden</h3>
 		<p><a href="thanks.php" class="btn btn-warning" role="button">Vote</a></p>
