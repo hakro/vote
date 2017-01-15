b1ec3ab2468ac02cbcf1154a06e91e80f5ff3b6c
Hakim ROUATBI <hakim@skale-5.com>
2017-01-15 12:44:03 +0100

    Simpsons vote

--- index.php
+++ index.php
@@ -29,9 +29,9 @@
 	<div class="row">
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/e.jpg">
+	      <img src="img/bart.png">
 	      <div class="caption">
-		<h3>Emily</h3>
+		<h3>Bart</h3>
 		<p><a href="thanks.php" class="btn btn-success" role="button">Vote</a></p>
 	      </div>
 	    </div>
@@ -39,9 +39,9 @@
 
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/t.jpg">
+	      <img src="img/homer.png">
 	      <div class="caption">
-		<h3>Tori</h3>
+		<h3>Homer</h3>
 		<p><a href="thanks.php" class="btn btn-primary" role="button">Vote</a></p>
 	      </div>
 	    </div>
@@ -49,9 +49,9 @@
 <!--
 	  <div class="col-md-2">
 	    <div class="thumbnail">
-	      <img src="img/k.jpg">
+	      <img src="img/lisa.png">
 	      <div class="caption">
-		<h3>Kayden</h3>
+		<h3>Lisa</h3>
 		<p><a href="thanks.php" class="btn btn-warning" role="button">Vote</a></p>
 	      </div>
 	    </div>
